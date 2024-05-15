1d0b8e9c62698825c3a576ba8298c2fef990fb5d
kr-mustali <mustali.c@knackroot.com>
2024-05-15 18:36:30 +0530

    app created

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const ua = require('universal-analytics');
+const express = require('express');
+const axios = require('axios');
 
-const tracker = ua('YOUR_TRACKING_ID');
+const app = express();
+const port = 3000;
 
-// Send an event
-tracker.event('Category', 'Action', 'Label', 'Value', (err) => {
-  if (err) {
-    console.error('Error sending event:', err);
-  } else {
-    console.log('Event sent successfully!');
+const MEASUREMENT_ID = 'G-DJ5LKLDVW6';
+
+app.get('/', async (req, res) => {
+  try {
+    const eventData = {
+      client_id: '123456',
+      events: [
+        {
+          name: 'homepage_view',
+        },
+      ],
+    };
+
+    await sendEvent(eventData);
+
+    res.send('Event sent to Google Analytics 4!');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error sending event to Google Analytics 4');
   }
+});
+
+// Function to send an event
+async function sendEvent(eventData) {
+  try {
+    await axios.post(`https://www.google-analytics.com/mp/collect?measurement_id=${MEASUREMENT_ID}`, eventData);
+    console.log('Event sent successfully');
+  } catch (error) {
+    console.error('Error sending event:', error.message);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
